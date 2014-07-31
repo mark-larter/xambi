54ae0ba8d2fba8a5f338b949a44ab64328938518
Mark Larter <mark@freeheelgroup.com>
2014-07-31 12:48:33 -0600

    Improve package management.

--- Core/Dto/Properties/AssemblyInfo.cs
+++ Core/Dto/Properties/AssemblyInfo.cs
@@ -1,27 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle ("Symbi.Core.Dto")]
-[assembly: AssemblyDescription ("Symbi data transfer interfaces.")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Troppus Software, an EchoStar corporation")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("mlarter")]
-[assembly: AssemblyTrademark("© 2014 Troppus Software, an EchoStar corporation")]
-[assembly: AssemblyCulture ("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
+// This assembly adheres to the Semantic Versioning spec found at http://semver.org.
+// Assembly version has the format "{Major}.{Minor}.{Patch}" for release versions,
+// or "{Major}.{Minor}.{Patch}-{Prerelease}" for pre-releases.
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyTitle("Symbi.Core.Dto")]
+[assembly: AssemblyDescription("Symbi data transfer interfaces.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Troppus Software, an EchoStar corporation")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("© 2014 Troppus Software, an EchoStar corporation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
